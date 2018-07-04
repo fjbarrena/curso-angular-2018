bc12a8a37917b8f821f9023bf063fd8481a79bb7
Francisco Javier Barrena <fjbarrena@gmail.com>
2018-07-04 18:45:48 +0200

    Angular Elements

---
+++ horizontal-timeline-webcomponent/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { TimelineComponent } from './timeline/timeline.component';
+
+@NgModule({
+  declarations: [
+    TimelineComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: []
+})
+export class AppModule {
+  constructor(private readonly injector: Injector) {}
+
+  ngDoBootstrap() {
+    const customTimeline = createCustomElement(TimelineComponent, { injector: this.injector });
+    customElements.define('paco-timeline', customTimeline);
+  }
+}
---
+++ horizontal-timeline-webcomponent/src/app/timeline/timeline.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  template: '<h1>Hola caracola</h1>'
+})
+export class TimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
